b2dbbb2bdbcbcb9629e7f798ad6b825cf91cec9c
Noah Maclean <noahmac5000@hotmail.com>
2025-12-03 15:42:45 +0000

    put whichkey config in opts rather than config function

--- dot_config/nvim/lua/plugins/which-key.lua
+++ dot_config/nvim/lua/plugins/which-key.lua
@@ -2,26 +2,47 @@
 return {
 	"folke/which-key.nvim",
 	event = "VeryLazy",
-	opts = {},
-	config = function()
-		local wk = require("which-key")
-		wk.add({
-			{ "<leader>f", group = " find files" }, -- shows leader f to be the file keybinds
-			{ "<leader>c", group = " code" }, -- shows leader c to be the code keybinds
-			{ "<leader>g", group = " git" }, -- shows leader g to be the git keybinds
-			{ "<leader>s", group = " search" }, -- shows leader s to be the search keybinds
-			{ "<leader>u", group = " hide" }, -- shows leader u to be the hide keybinds
-			{ "<leader>p", group = " fun!", icon = "" }, -- shows leader p to be the fun keybinds
-			{ "<leader>m", group = " markdown", icon = "" }, -- shows leader m to be the markdown keybinds
-			{ "<leader>t", group = " markdown table", icon = "" }, -- shows leader t to be the markdown table keybinds
-			{ "<leader>h", group = " markdown headings", icon = "󰰀" },
-			{ "<leader>mf", group = " markdown footnotes" },
-			{ "<leader>mQ", group = " markdown quotes" },
-			{ "<leader>pd", group = " ducks", icon = "󰇥" }, -- shows leader pd to be the duck keybinds
-		})
-		wk.setup({
-			-- preset options are classic, modern and helix
-			preset = "helix",
-		})
-	end,
+	opts = {
+		-- preset options are classic, modern and helix
+		preset = "helix",
+		spec = {
+			{
+				mode = { "n", "x" },
+				{ "<leader>f", group = " find files" }, -- shows leader f to be the file keybinds
+				{ "<leader>c", group = " code" }, -- shows leader c to be the code keybinds
+				{ "<leader>g", group = " git" }, -- shows leader g to be the git keybinds
+				{ "<leader>s", group = " search" }, -- shows leader s to be the search keybinds
+				{ "<leader>u", group = " hide" }, -- shows leader u to be the hide keybinds
+				{ "<leader>p", group = " fun!", icon = "" }, -- shows leader p to be the fun keybinds
+				{ "<leader>m", group = " markdown", icon = "" }, -- shows leader m to be the markdown keybinds
+				{ "<leader>t", group = " markdown table", icon = "" }, -- shows leader t to be the markdown table keybinds
+				{ "<leader>h", group = " markdown headings", icon = "󰰀" },
+				{ "<leader>mf", group = " markdown footnotes" },
+				{ "<leader>mQ", group = " markdown quotes" },
+				{ "<leader>pd", group = " ducks", icon = "󰇥" }, -- shows leader pd to be the duck keybinds
+			},
+		},
+	},
+
+	-- config = function()
+	-- 	local wk = require("which-key")
+	-- 	wk.add({
+	-- 		{ "<leader>f", group = " find files" }, -- shows leader f to be the file keybinds
+	-- 		{ "<leader>c", group = " code" }, -- shows leader c to be the code keybinds
+	-- 		{ "<leader>g", group = " git" }, -- shows leader g to be the git keybinds
+	-- 		{ "<leader>s", group = " search" }, -- shows leader s to be the search keybinds
+	-- 		{ "<leader>u", group = " hide" }, -- shows leader u to be the hide keybinds
+	-- 		{ "<leader>p", group = " fun!", icon = "" }, -- shows leader p to be the fun keybinds
+	-- 		{ "<leader>m", group = " markdown", icon = "" }, -- shows leader m to be the markdown keybinds
+	-- 		{ "<leader>t", group = " markdown table", icon = "" }, -- shows leader t to be the markdown table keybinds
+	-- 		{ "<leader>h", group = " markdown headings", icon = "󰰀" },
+	-- 		{ "<leader>mf", group = " markdown footnotes" },
+	-- 		{ "<leader>mQ", group = " markdown quotes" },
+	-- 		{ "<leader>pd", group = " ducks", icon = "󰇥" }, -- shows leader pd to be the duck keybinds
+	-- 	})
+	-- 	wk.setup({
+	-- 		-- preset options are classic, modern and helix
+	-- 		preset = "helix",
+	-- 	})
+	-- end,
 }
