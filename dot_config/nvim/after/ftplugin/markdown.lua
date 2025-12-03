aac7f33da6c67545debb8bcdcbc156c43a138746
Noah Maclean <noahmac5000@hotmail.com>
2025-12-03 01:30:25 +0000

    added after/ftplugin/ to configure file type specific settings

---
+++ dot_config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,8 @@
+-- allow folding on headings, bullets, etc.
+vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.wo.foldmethod = "expr"
+
+-- start with all folds closed
+vim.wo.foldlevel = 99
+-- set spell checking in markdown files
+vim.wo.spell = true
