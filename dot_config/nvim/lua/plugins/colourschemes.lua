f1af302860ac54f6e5bb5b32826b547a8e5c1b07
Noah Maclean <noahmac5000@hotmail.com>
2025-12-07 20:03:36 +0000

    added transparent.nvim for toggleable transparency

--- dot_config/nvim/lua/plugins/colourschemes.lua
+++ dot_config/nvim/lua/plugins/colourschemes.lua
@@ -3,6 +3,36 @@
 -- can be changed with "<leader>C"
 return {
 	{
+		"xiyaowong/transparent.nvim",
+		config = function()
+			local transparent = require("transparent")
+
+			vim.keymap.set("n", "<leader>t", "<cmd>TransparentToggle<CR>", { desc = "Toggle [T]ransparency" })
+
+			transparent.clear_prefix("Snacks")
+			transparent.clear_prefix("WhichKey")
+
+			transparent.setup({
+				extra_groups = {
+					-- Standard floating windows (often used as fallback)
+					"NormalFloat",
+					"FloatBorder",
+				},
+				exclude_groups = {  "SnacksPickerListCursorLine", "SnacksPickerPreviewCursorLine" },
+			})
+
+			-- vim.api.nvim_create_autocmd("ColorScheme", {
+			-- 	pattern = "*",
+			-- 	callback = function()
+			-- 		transparent.clear_prefix("Snacks")
+			-- 		transparent.clear_prefix("WhichKey")
+			-- 		vim.api.nvim_set_hl(0, "SnacksPickerListCursorLine", { link = "Visual" })
+			-- 		vim.api.nvim_set_hl(0, "SnacksPickerPreviewCursorLine", { link = "Visual" })
+			-- 	end,
+			-- })
+		end,
+	},
+	{
 		-- tokyonight
 		"folke/tokyonight.nvim",
 		lazy = false,
@@ -15,35 +45,38 @@ return {
 			-- },
 		},
 		config = function()
-			require("tokyonight").setup({
-				transparent = true,
-				styles = {
-					sidebars = "transparent",
-					floats = "transparent",
-				},
-			})
+			-- require("tokyonight").setup({
+			-- 		transparent = true,
+			-- 		styles = {
+			-- 			sidebars = "transparent",
+			-- 			floats = "transparent",
+			-- 		},
+			-- 	})
 			vim.cmd.colorscheme("tokyonight")
 		end,
 	},
 	{
 		-- onedark
 		"navarasu/onedark.nvim",
-		config = function()
-			require("onedark").setup({
-				transparent = true,
-			})
-		end,
+		-- config = function()
+		-- 	require("onedark").setup({
+		-- 		transparent = true,
+		-- 	})
+		-- end,
 	},
 	{
 		-- catppuccin
 		"catppuccin/nvim",
-		config = function()
-			require("catppuccin").setup({
-				transparent_background = true,
-				float = {
-					transparent = true,
-				},
-			})
-		end,
+		-- config = function()
+		-- 	require("catppuccin").setup({
+		-- 		transparent_background = true,
+		-- 		float = {
+		-- 			transparent = true,
+		-- 		},
+		-- 	})
+		-- end,
+	},
+	{
+		"EdenEast/nightfox.nvim",
 	},
 }
