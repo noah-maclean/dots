a725e4e11fc6e759b1162c9b628a520ada9196b0
Noah Maclean <noahmac5000@hotmail.com>
2025-12-20 01:14:32 +0000

    switched to main branch of treesitter as master is no longer being update. added treesitter modules to reinstate the ensure installed features and more, added textobjects and context for more functionality

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,16 +1,43 @@
 -- treesitter - basic language functionality such as highlighting using tree-sitter
 return {
+	-- {
+	-- 	"nvim-treesitter/nvim-treesitter",
+	--        lazy = false,
+	--        branch = "master",
+	-- 	build = ":TSUpdate",
+	-- 	config = function()
+	-- 		local config = require("nvim-treesitter.configs")
+	-- 		config.setup({
+	-- 			auto_install = true,
+	-- 			highlight = { enable = true },
+	-- 			indent = { enable = true },
+	-- 			ensure_installed = {
+	-- 				"lua",
+	-- 				"python",
+	-- 				"c",
+	-- 				"html",
+	-- 				"css",
+	-- 				"javascript",
+	-- 				"markdown",
+	-- 				"markdown_inline",
+	-- 				"vim",
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
 	{
 		"nvim-treesitter/nvim-treesitter",
-        lazy = false,
-        branch = "master",
+		branch = "main",
+		lazy = false,
 		build = ":TSUpdate",
+	},
+	{
+        -- reinstates the previous treesitter functionality, like ensure installed, auto install, etc.
+		"MeanderingProgrammer/treesitter-modules.nvim",
+		lazy = false,
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
 		config = function()
-			local config = require("nvim-treesitter.configs")
-			config.setup({
-				auto_install = true,
-				highlight = { enable = true },
-				indent = { enable = true },
+			require("treesitter-modules").setup({
 				ensure_installed = {
 					"lua",
 					"python",
@@ -22,7 +49,49 @@ return {
 					"markdown_inline",
 					"vim",
 				},
+				-- auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+				-- fold = { enable = true },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<C-space>",
+						node_incremental = "<C-space>",
+						scope_incremental = false,
+						node_decremental = "<bs>",
+					},
+				},
 			})
 		end,
+	},
+	{
+        -- textobjects allows for more textobjects to be used than standard vim/nvim
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "main",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require("nvim-treesitter-textobjects").setup({
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = "@function.outer", -- Select around function
+						["if"] = "@function.inner", -- Select inner function
+						["ac"] = "@class.outer", -- Select around class
+						["ic"] = "@class.inner", -- Select inner class
+					},
+				},
+			})
+		end,
+	},
+	{
+        -- context adds "sticky headers" for classes, functions, etc.
+		"nvim-treesitter/nvim-treesitter-context",
+		lazy = false,
+		opts = {
+			enable = true,
+			max_lines = 3,
+		},
 	},
 }
