17a74de86ef89316776883d9518fafb9ef9fdcba
Noah Maclean <noahmac5000@hotmail.com>
2026-01-20 23:04:37 +0000

    added an autocommand to hide line numbers in alpha nvim

--- dot_config/nvim/lua/autocmds.lua
+++ dot_config/nvim/lua/autocmds.lua
@@ -1,9 +1,16 @@
 -- highlight on yank
-vim.api.nvim_create_autocmd('TextYankPost', {
-  group = vim.api.nvim_create_augroup('highlight_yank', {}),
-  desc = 'Hightlight selection on yank',
-  pattern = '*',
-  callback = function()
-    vim.hl.on_yank { higroup = 'Visual', timeout = 250 }
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = vim.api.nvim_create_augroup("highlight_yank", {}),
+	desc = "Hightlight selection on yank",
+	pattern = "*",
+	callback = function()
+		vim.hl.on_yank({ higroup = "Visual", timeout = 250 })
+	end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "alpha",
+	callback = function()
+        vim.opt_local.statuscolumn = ""
+	end,
 })
