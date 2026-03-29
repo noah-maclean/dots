c2bf2cc8728c2cf4219fe46286e371f947e934b2
Noah Maclean <noahmac5000@hotmail.com>
2026-03-29 02:00:39 +0100

    configured nvim outline and changed the keybinds

--- dot_config/nvim/lua/plugins/outline.lua
+++ dot_config/nvim/lua/plugins/outline.lua
@@ -4,8 +4,19 @@ return {
 	lazy = true,
 	cmd = { "Outline", "OutlineOpen" },
 	keys = {
-		{ "<leader>o", "<cmd>Outline<CR>", desc = "Toggle [O]utline" },
+		{ "<leader>o", "<cmd>Outline!<CR>", desc = "Toggle [O]utline" },
+		{ "<leader>O", "<cmd>Outline<CR>", desc = "Toggle [O]utline and focus)" },
 	},
 	opts = {
+		outline_window = {
+			show_cursorline = true,
+			hide_cursor = true,
+		},
+		width = 25,
+		auto_width = {
+			enabled = true,
+			max_width = 35,
+		},
+        relative_width = true,
 	},
 }
